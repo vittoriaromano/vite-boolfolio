ed1b610cca3fe801d00659a17735ff9914823e39
vittoriaromano <vittoria5005@gmail.com>
2023-05-28 22:41:04 +0400

    Chiamata Api

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,44 @@
 <script>
-import HelloApp from './components/HelloApp.vue';
+import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
 
 export default {
   name:'App',
+  data() {
+    return {
+      apiBaseUrl: 'http://127.0.0.1:8000/api/',
+      apiUrls: {
+        projects: 'projects'
+      },
+      projects: []
+    }
+  },
   components: {
-    HelloApp,
     AppHeader,
     AppMain,
     AppFooter
+  },
+  methods:{
+    getProject(){
+      axios.get(this.apiBaseUrl + this.apiUrls.projects).then((response)=>{
+        this.projects = response.data.results;
+      }).catch((error)=>{
+        console.log(error);
+      })
+    }
+
+  },
+  created() {
+    this.getProject();
   }
 }
 </script>
 
 <template>
   <AppHeader />
-  <AppMain />
+  <AppMain :data="projects" />
   <AppFooter />
 </template>
 
