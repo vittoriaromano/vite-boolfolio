f6e856fb477b46688e0f297146668ee27adec215
vittoriaromano <vittoria5005@gmail.com>
2023-05-29 19:26:08 +0400

    Vue router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue';
+import { router } from './router';
 import * as bootstrap from 'bootstrap';
 import './assets/scss/main.scss';
 
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
